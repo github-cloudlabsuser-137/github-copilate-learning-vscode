0fe9647aa3ce781f96e5c1626437cb8da6a1d1bb
github-cloudlabsuser-137 <150014331+github-cloudlabsuser-137@users.noreply.github.com>
2023-12-06 18:39:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,33 @@
+function skillsMember() {
+    return {
+        name: 'Member',
+        skills: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'React',
+            'Node.js',
+            'Express',
+            'MongoDB',
+            'Python',
+            'C++',
+            'Java',
+            'Git',
+            'GitHub',
+            'VS Code',
+            'Heroku',
+            'Netlify',
+            'Figma',
+            'Adobe XD',
+            'Adobe Photoshop',
+            'Adobe Illustrator',
+            'Adobe InDesign',
+            'Adobe After Effects',
+            'Adobe Premiere Pro',
+            'Adobe Lightroom',
+            'Adobe Audition',
+            'Microsoft Office',
+            'Google Suite'
+        ]
+    }
+}
